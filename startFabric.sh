69a5c9f4f521223c61d4e75fd05822901a2fdcf3
LorranSutter <sutterlorran@gmail.com>
2020-06-30 22:33:03 +0000

    Update gitignore | update startFabric

--- startFabric.sh
+++ startFabric.sh
@@ -10,7 +10,7 @@ set -e
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
 starttime=$(date +%s)
-CC_SRC_LANGUAGE=${1:-"go"}
+CC_SRC_LANGUAGE=${1:-"javascript"}
 CC_SRC_LANGUAGE=$(echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:])
 if [ "$CC_SRC_LANGUAGE" != "go" -a "$CC_SRC_LANGUAGE" != "golang" -a "$CC_SRC_LANGUAGE" != "java" \
   -a "$CC_SRC_LANGUAGE" != "javascript" -a "$CC_SRC_LANGUAGE" != "typescript" ]; then
@@ -26,8 +26,8 @@ rm -rf api/wallet/*
 
 # launch network; create channel and join peer to channel
 cd ./test-network
-# ./network.sh down
-# ./network.sh up createChannel -ca -s couchdb
+./network.sh down
+./network.sh up createChannel -ca -s couchdb
 ./network.sh deployCC -l ${CC_SRC_LANGUAGE}
 
 cat <<EOF
