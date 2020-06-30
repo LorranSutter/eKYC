b6315cb0fcd0efd74693b5964d0a18dad3c6eee2
LorranSutter <sutterlorran@gmail.com>
2020-06-30 22:15:31 +0000

    Include test-network scripts | change startFabric

--- startFabric.sh
+++ startFabric.sh
@@ -11,28 +11,24 @@ set -e
 export MSYS_NO_PATHCONV=1
 starttime=$(date +%s)
 CC_SRC_LANGUAGE=${1:-"go"}
-CC_SRC_LANGUAGE=`echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:]`
+CC_SRC_LANGUAGE=$(echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:])
 if [ "$CC_SRC_LANGUAGE" != "go" -a "$CC_SRC_LANGUAGE" != "golang" -a "$CC_SRC_LANGUAGE" != "java" \
- -a  "$CC_SRC_LANGUAGE" != "javascript"  -a "$CC_SRC_LANGUAGE" != "typescript" ] ; then
+  -a "$CC_SRC_LANGUAGE" != "javascript" -a "$CC_SRC_LANGUAGE" != "typescript" ]; then
 
-	echo The chaincode language ${CC_SRC_LANGUAGE} is not supported by this script
- 	echo Supported chaincode languages are: go, java, javascript, and typescript
- 	exit 1
+  echo The chaincode language ${CC_SRC_LANGUAGE} is not supported by this script
+  echo Supported chaincode languages are: go, java, javascript, and typescript
+  exit 1
 
 fi
 
 # clean out any old identites in the wallets
-rm -rf javascript/wallet/*
-rm -rf java/wallet/*
-rm -rf typescript/wallet/*
-rm -rf go/wallet/*
+rm -rf api/wallet/*
 
 # launch network; create channel and join peer to channel
-pushd ../test-network
-./network.sh down
-./network.sh up createChannel -ca -s couchdb
+cd ./test-network
+# ./network.sh down
+# ./network.sh up createChannel -ca -s couchdb
 ./network.sh deployCC -l ${CC_SRC_LANGUAGE}
-popd
 
 cat <<EOF
 
