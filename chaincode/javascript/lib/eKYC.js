fc02bd955c4b313a914b3d2fd9a602d88836b140
LorranSutter <sutterlorran@gmail.com>
2020-07-01 19:16:15 +0000

    Chaincode initial commit

--- chaincode/javascript/lib/eKYC.js
+++ chaincode/javascript/lib/eKYC.js
@@ -12,74 +12,74 @@ class eKYC extends Contract {
 
     async initLedger(ctx) {
         console.info('============= START : Initialize Ledger ===========');
-        // const cars = [
-        //     {
-        //         color: 'blue',
-        //         make: 'Toyota',
-        //         model: 'Prius',
-        //         owner: 'Tomoko',
-        //     },
-        //     {
-        //         color: 'red',
-        //         make: 'Ford',
-        //         model: 'Mustang',
-        //         owner: 'Brad',
-        //     },
-        //     {
-        //         color: 'green',
-        //         make: 'Hyundai',
-        //         model: 'Tucson',
-        //         owner: 'Jin Soo',
-        //     },
-        //     {
-        //         color: 'yellow',
-        //         make: 'Volkswagen',
-        //         model: 'Passat',
-        //         owner: 'Max',
-        //     },
-        //     {
-        //         color: 'black',
-        //         make: 'Tesla',
-        //         model: 'S',
-        //         owner: 'Adriana',
-        //     },
-        //     {
-        //         color: 'purple',
-        //         make: 'Peugeot',
-        //         model: '205',
-        //         owner: 'Michel',
-        //     },
-        //     {
-        //         color: 'white',
-        //         make: 'Chery',
-        //         model: 'S22L',
-        //         owner: 'Aarav',
-        //     },
-        //     {
-        //         color: 'violet',
-        //         make: 'Fiat',
-        //         model: 'Punto',
-        //         owner: 'Pari',
-        //     },
-        //     {
-        //         color: 'indigo',
-        //         make: 'Tata',
-        //         model: 'Nano',
-        //         owner: 'Valeria',
-        //     },
-        //     {
-        //         color: 'brown',
-        //         make: 'Holden',
-        //         model: 'Barina',
-        //         owner: 'Shotaro',
-        //     },
-        // ];
+        const cars = [
+            {
+                color: 'blue',
+                make: 'Toyota',
+                model: 'Prius',
+                owner: 'Tomoko',
+            },
+            {
+                color: 'red',
+                make: 'Ford',
+                model: 'Mustang',
+                owner: 'Brad',
+            },
+            {
+                color: 'green',
+                make: 'Hyundai',
+                model: 'Tucson',
+                owner: 'Jin Soo',
+            },
+            {
+                color: 'yellow',
+                make: 'Volkswagen',
+                model: 'Passat',
+                owner: 'Max',
+            },
+            {
+                color: 'black',
+                make: 'Tesla',
+                model: 'S',
+                owner: 'Adriana',
+            },
+            {
+                color: 'purple',
+                make: 'Peugeot',
+                model: '205',
+                owner: 'Michel',
+            },
+            {
+                color: 'white',
+                make: 'Chery',
+                model: 'S22L',
+                owner: 'Aarav',
+            },
+            {
+                color: 'violet',
+                make: 'Fiat',
+                model: 'Punto',
+                owner: 'Pari',
+            },
+            {
+                color: 'indigo',
+                make: 'Tata',
+                model: 'Nano',
+                owner: 'Valeria',
+            },
+            {
+                color: 'brown',
+                make: 'Holden',
+                model: 'Barina',
+                owner: 'Shotaro',
+            },
+        ];
 
-        // for (let i = 0; i < cars.length; i++) {
-        //     cars[i].docType = 'car';
-        //     await ctx.stub.putState('CAR' + i, Buffer.from(JSON.stringify(cars[i])));
-        //     console.info('Added <--> ', cars[i]);
-        // }
+        for (let i = 0; i < cars.length; i++) {
+            cars[i].docType = 'car';
+            await ctx.stub.putState('CAR' + i, Buffer.from(JSON.stringify(cars[i])));
+            console.info('Added <--> ', cars[i]);
+        }
         console.info('============= END : Initialize Ledger ===========');
     }
 
@@ -107,24 +107,24 @@ class eKYC extends Contract {
     //     console.info('============= END : Create Car ===========');
     // }
 
-    // async queryAllCars(ctx) {
-    //     const startKey = '';
-    //     const endKey = '';
-    //     const allResults = [];
-    //     for await (const {key, value} of ctx.stub.getStateByRange(startKey, endKey)) {
-    //         const strValue = Buffer.from(value).toString('utf8');
-    //         let record;
-    //         try {
-    //             record = JSON.parse(strValue);
-    //         } catch (err) {
-    //             console.log(err);
-    //             record = strValue;
-    //         }
-    //         allResults.push({ Key: key, Record: record });
-    //     }
-    //     console.info(allResults);
-    //     return JSON.stringify(allResults);
-    // }
+    async queryAllCars(ctx) {
+        const startKey = '';
+        const endKey = '';
+        const allResults = [];
+        for await (const {key, value} of ctx.stub.getStateByRange(startKey, endKey)) {
+            const strValue = Buffer.from(value).toString('utf8');
+            let record;
+            try {
+                record = JSON.parse(strValue);
+            } catch (err) {
+                console.log(err);
+                record = strValue;
+            }
+            allResults.push({ Key: key, Record: record });
+        }
+        console.info(allResults);
+        return JSON.stringify(allResults);
+    }
 
     // async changeCarOwner(ctx, carNumber, newOwner) {
     //     console.info('============= START : changeCarOwner ===========');
