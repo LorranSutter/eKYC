852dba46ad26b544386271bd2efba8b633247b1d
LorranSutter <sutterlorran@gmail.com>
2020-07-04 00:00:46 +0000

    Add TODOs | validate using relations

--- chaincode/javascript/lib/eKYC.js
+++ chaincode/javascript/lib/eKYC.js
@@ -7,11 +7,17 @@
 'use strict';
 
 const { Contract } = require('fabric-contract-api');
+// TODO maybe use shim to return success messages
+// Example: return shim.success(Buffer.from('saveData Successful!'));
+// const shim = require('fabric-shim');
 
 const initialClientData = require('../data/initialClientData.json');
 const initialFIData = require('../data/initialFIData.json');
 
 class eKYC extends Contract {
+
+    // TODO define an id counter for client
+    // TODO define an id counter for fi
 
     async initLedger(ctx) {
         console.info('============= START : Initialize Ledger ===========');
@@ -20,19 +26,29 @@ class eKYC extends Contract {
 
         for (let i = 0; i < clients.length; i++) {
             clients[i].docType = 'client';
+            // TODO Use id counter for client
             await ctx.stub.putState('CLIENT' + i, Buffer.from(JSON.stringify(clients[i])));
             console.info('Added <--> ', clients[i]);
         }
 
         for (let i = 0; i < fis.length; i++) {
             fis[i].docType = 'fi';
+            // TODO Use id counter for fi
             await ctx.stub.putState('FI' + i, Buffer.from(JSON.stringify(fis[i])));
             console.info('Added <--> ', fis[i]);
         }
         console.info('============= END : Initialize Ledger ===========');
     }
 
-    async getClientData(ctx, clientId) {
+    async getClientData(ctx, clientId, fiId = null) {
+
+        if (fiId) {
+            const relations = await this.getRelationByFi(fiId);
+            if (!relations.includes(clientId)) {
+                return null;
+            }
+        }
+
         const clientAsBytes = await ctx.stub.getState(clientId);
         if (!clientAsBytes || clientAsBytes.length === 0) {
             throw new Error(`${clientId} does not exist`);
@@ -81,22 +97,45 @@ class eKYC extends Contract {
     async approve(ctx, clientId, fiId) {
         console.log('======== START : Approve financial institution for client data access ==========');
 
-        // TODO think if it is necessary create a relation fi~client too
         const clientFiIndexKey = await ctx.stub.createCompositeKey('clientId~fiId', [clientId.toString(), fiId.toString()]);
+        const fiClientIndexKey = await ctx.stub.createCompositeKey('fiId~clientId', [fiId.toString(), clientId.toString()]);
 
         if (!clientFiIndexKey) {
             throw new Error('Composite key: clientFiIndexKey is null');
         }
 
-        console.log(clientFiIndexKey);
+        if (!fiClientIndexKey) {
+            throw new Error('Composite key: fiClientIndexKey is null');
+        }
 
         await ctx.stub.putState(clientFiIndexKey, Buffer.from('\u0000'));
+        await ctx.stub.putState(fiClientIndexKey, Buffer.from('\u0000'));
         console.log('======== END : Approve financial institution for client data access =========');
     }
 
-    async getRelation(ctx, clientId) {
+    async getRelationByClient(ctx, clientId) {
 
         const relationResultsIterator = await ctx.stub.getStateByPartialCompositeKey('clientId~fiId', [clientId.toString()]);
+
+        let relationsArray = [];
+        while (true) {
+
+            const responseRange = await relationResultsIterator.next();
+
+            if (!responseRange || !responseRange.value || !responseRange.value.key) {
+                return JSON.stringify(relationsArray);
+            }
+
+            const { attributes } = await ctx.stub.splitCompositeKey(responseRange.value.key);
+
+            relationsArray.push(attributes[1]);
+
+        }
+    }
+
+    async getRelationByFi(ctx, fiId) {
+
+        const relationResultsIterator = await ctx.stub.getStateByPartialCompositeKey('fiId~clientId', [fiId.toString()]);
 
         let relationsArray = [];
         while (true) {
